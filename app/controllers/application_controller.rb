58277e4e6f4057c903570c98d3be820b4a37b363
laureli mallek <laureli@artemis.net>
2013-11-30 22:42:15 -0800

    adding cart, expanding test, adding visit counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :date_loaded
+
+  before_filter :set_date
+
+  def set_date
+    @date_loaded = Time.now
+  end
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id 
+    cart
+  end
+  
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.order(:title)
+    @counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter+=1
+    session[:counter] = @counter
   end
+
+  # def increment_count
+  #   session[:counter] || 0
+  #   session[:counter] += 1
+  # end
 end
